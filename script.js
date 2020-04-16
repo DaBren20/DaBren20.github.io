f7e28c61742762bd709a1966bf845c64d233f6ab
DaBren20 <58720854+DaBren20@users.noreply.github.com>
2020-04-16 19:58:00 -0400

    first commit
    Website

---
+++ script.js
@@ -0,0 +1,8 @@
+/** Add any JavaScript you need to this file. */
+
+function darkMode() {
+    var element = document.body;
+    var element2 = document.toilet1;
+    element.classList.toggle("dark-mode");
+    element2.fontcolor("white");
+}
